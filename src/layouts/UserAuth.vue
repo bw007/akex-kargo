d96a449a3d0cfeda5eb8b19f051ff0b17ed32be2
bw007 <mr.abdulaziz00791@gmail.com>
2024-04-09 14:27:16 +0500

    Auth layout

---
+++ src/layouts/UserAuth.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="auth">
+    <header class="auth__header">
+      <router-link to="/auth/login">sign in</router-link>
+      <router-link to="/">
+        
+      </router-link>
+    </header>
+    
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="css">
+
+</style>
