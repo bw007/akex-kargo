b5e36405a99a8fb86f9d175803af9e01cae33ba8
bw007 <mr.abdulaziz00791@gmail.com>
2024-04-13 16:45:09 +0500

    User auth layout

--- src/layouts/UserAuth.vue
+++ src/layouts/UserAuth.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="auth">
     <header class="auth__header">
-      <router-link to="/auth/login">sign in</router-link>
-      <router-link to="/">
-        
+      <router-link class="auth__link" to="/auth/login">sign in</router-link>
+      <router-link class="auth__logo" to="/auth/login">
+        <img src="@/assets/imgs/logo.svg" />
       </router-link>
     </header>
-    
+    <el-divider />
+    <section class="auth__form">
+      <RouterView />
+    </section>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="css">
-
+<style lang="css" scoped>
+.auth__header {
+  display: flex;
+  align-items: center;
+  padding: 12px 28px;
+  position: relative;
+}
+.auth__link {
+  position: absolute;
+}
+.auth__logo {
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+}
+.el-divider {
+  margin: 0;
+}
+.auth__form {
+  padding: 12px 28px;
+  display: flex;
+  align-items: center;
+}
 </style>
