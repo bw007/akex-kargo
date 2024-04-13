6d9a5401983716b28405dc1e548cec7b8389ddbc
bw007 <mr.abdulaziz00791@gmail.com>
2024-04-13 16:45:19 +0500

    Updated

--- src/views/auth/UserLogin.vue
+++ src/views/auth/UserLogin.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>
-    dsdsd
-  </div>
+  <el-form label-position="top">
+    <el-text class="title">Log in</el-text>
+    <el-form-item label="Email address or user name">
+      <el-input v-model="user.email" />
+    </el-form-item>
+    
+    <el-form-item label="Password">
+      <el-input v-model="user.password" show />
+    </el-form-item>
+    <el-form-item>
+      <el-checkbox v-model="user.remember">Not disabled</el-checkbox>
+    </el-form-item>
+  </el-form>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
 
-<style lang="css">
-  
+const user = ref({});
+</script>
+
+<style lang="css" scoped>
+.el-form {
+
+}
+.title {
+  font-size: 24px;
+}
+.el-form-item {
+
+}
 </style>
