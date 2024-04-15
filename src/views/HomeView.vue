432ffa49a317a3d77a4250b33d0b67ef58ebcac3
bw007 <abdulaziz00791@gmail.com>
2024-04-15 09:51:40 +0500

    Home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
+  <div class="wrapper">
     Dashboard
+    <router-link to="/auth/login">Log In</router-link>
   </div>
 </template>
 
 <script setup></script>
 
 <style lang="css">
-  
+  .wrapper {
+    padding: 10px 28px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 </style>
