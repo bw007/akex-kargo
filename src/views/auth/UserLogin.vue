aa846b25a48eb1a648297433376607c58ff047e4
bw007 <abdulaziz00791@gmail.com>
2024-04-16 00:47:51 +0500

    User data inputs and styles, optimization

--- src/views/auth/UserLogin.vue
+++ src/views/auth/UserLogin.vue
@@ -1,33 +1,43 @@
 <template>
   <el-form label-position="top">
-    <el-text class="title">Log in</el-text>
-    <el-form-item label="Email address or user name">
-      <el-input v-model="user.email" />
+    <el-text class="title">Tizimga kirish</el-text>
+    <el-form-item label="Email yoki foydalanuvchi nomi">
+      <el-input 
+        v-model="user.email"
+        size="large"
+      />
     </el-form-item>
-    <el-form-item label="Password">
+    <el-form-item label="Mahfiy kalit">
       <el-input 
         v-model="user.password" 
         show-password
         type="password"
+        size="large"
       />
     </el-form-item>
     <el-form-item>
-      <el-checkbox v-model="user.remember" label="Remember me" />
+      <el-checkbox
+        v-model="user.remember"
+        label="Meni eslab qoling"
+      />
     </el-form-item>
     <el-form-item>
-      <el-text class="policy">
-        By continuing, you agree to the
-        <router-link to="/terms">Terms of use</router-link>
-        and
-        <router-link to="/policy">Privacy Policy</router-link>
-      </el-text>
-      <el-button size="large" type="primary" round>Log in</el-button>
+      <!-- <el-text class="policy">
+        Davom etish orqali
+        <router-link to="/terms">Foydalanish shartlari</router-link>
+        va
+        <router-link to="/policy">Maxfiylik siyosatiga</router-link>
+        rozilik bildirasiz
+      </el-text> -->
+      <el-button size="large" type="primary" round>Kirish</el-button>
     </el-form-item>
     <div class="form__links">
-      <router-link to="/auth/forget">Forget your password</router-link>
       <el-text>
-        Don't have an acount?
-        <router-link to="/auth/reg">Sign up</router-link>
+        <router-link to="/auth/forget">Mahfiy kalitingizni unutdingizmi?</router-link>
+      </el-text>
+      <el-text>
+        Hisobingiz yo'qmi?
+        <router-link to="/auth/reg">Ro'yxatdan o'ting</router-link>
       </el-text>
     </div>
   </el-form>
@@ -37,7 +47,8 @@
 import { ref } from 'vue';
 
 const user = ref({
-  user: "",
+  email: "",
+  password: "",
   remember: false,
 });
 
@@ -45,36 +56,40 @@ const user = ref({
 
 <style lang="css" scoped>
 .el-form {
-  width: 100%;
-  max-width: 380px;
+  width: 380px;
   min-width: 220px;
 }
 .el-text.title {
   font-size: 24px;
   display: inline-block;
-  margin-bottom: 28px;
+  margin-bottom: 24px;
   font-weight: 500;
   color: #333333;
 }
-.el-text.policy {
-  font-size: 12px;
+/* .el-text.policy {
+  font-size: 13px;
+  line-height: 18px;
   text-align: center;
-}
+} */
 .el-form-item {
-  margin-bottom: 14px;
+  margin-bottom: 16px;
 }
 .form__links {
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 8px;
-  font-size: 14px;
-} 
+}
+.form__links a {
+  color: #333333;
+}
+.form__links .el-text {
+  margin-bottom: 10px;
+}
 .el-button {
   font-weight: 500;
   display: inline-block;
   width: 100%;
-  font-size: 15px;
+  font-size: 16px;
   margin-top: 10px;
   margin-bottom: 10px;
 }
