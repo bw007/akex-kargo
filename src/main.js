e7faa2ff3a0ee0a66d3d8d2f584380d760db7721
bw007 <mr.abdulaziz00791@gmail.com>
2024-04-19 17:11:01 +0500

    Pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import "./assets/styles/main.css"
+
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
+
 import router from "./router";
 
 import ElementPlus from 'element-plus'
@@ -13,6 +16,7 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
+app.use(createPinia())
 app.use(ElementPlus)
 app.use(router)
 app.mount("#app")
