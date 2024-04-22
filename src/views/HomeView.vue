06509529df698ce010129f504699fe6cf1765657
bw007 <abdulaziz00791@gmail.com>
2024-04-22 07:59:53 +0500

    Home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,39 @@
 <template>
-  <div class="wrapper">
-    Dashboard
-    <div class="auth__block">
-      <router-link to="/auth/signin">Sign In</router-link>
-      <router-link to="/auth/signup">Sign Up</router-link>
-    </div>
-  </div>
+  <section>
+    <h1>{{ $route.meta.title }}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio cupiditate, pariatur saepe maiores ducimus
+      adipisci? Consequatur, esse. Minus neque quas veniam deleniti vitae asperiores dolorem inventore adipisci sint nam
+      maiores veritatis tenetur officiis quaerat praesentium saepe provident, autem natus corrupti sapiente, atque
+      harum! Adipisci dolorum perspiciatis est, architecto illo repudiandae consectetur tenetur neque illum
+      reprehenderit odit similique enim ab excepturi id explicabo ea vitae suscipit reiciendis atque saepe consequatur.
+      Quia rem quo, similique optio quod quasi vitae dolores fuga magnam culpa! Repellendus consequatur recusandae,
+      molestiae accusamus corporis qui reprehenderit eaque veniam praesentium ratione. Neque vitae, voluptate molestiae
+      dolor ducimus maxime pariatur repudiandae officia quas cum nesciunt ipsa fugiat veniam eius? Animi nobis expedita
+      error dolore, impedit, voluptas saepe obcaecati exercitationem quaerat alias quasi molestias sapiente commodi
+      consequuntur sunt, debitis laudantium corporis aliquid. Veritatis adipisci corporis aut exercitationem sequi velit
+      iusto voluptatem ex incidunt! Consequuntur a quas doloribus quisquam nemo, cumque modi. Harum dolor rerum
+      blanditiis eius ab ipsam nulla corrupti, est obcaecati accusamus non facere veritatis quibusdam recusandae,
+      mollitia totam consectetur omnis, aut accusantium ducimus dolore ad dolores illo! Ut maxime doloremque alias
+      itaque nam labore blanditiis cum, provident culpa? Sequi, cum repellat quae quibusdam molestiae laborum
+      voluptatibus voluptate deserunt!</p>
+  </section>
 </template>
 
 <script setup></script>
 
 <style lang="css">
-  .wrapper {
-    padding: 10px 28px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .auth__block {
-    display: flex;
-    gap: 10px;
-  }
+.wrapper {
+  padding: 10px 28px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.auth__block {
+  display: flex;
+  gap: 10px;
+}
+h1 {
+  margin-bottom: 50px;
+}
 </style>
