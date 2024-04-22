7de49ddc7aa2b7169ede022a4928d723b3355fa0
bw007 <mr.abdulaziz00791@gmail.com>
2024-04-22 17:08:34 +0500

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,35 @@
 <template>
   <section>
     <h1>{{ $route.meta.title }}</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio cupiditate, pariatur saepe maiores ducimus
-      adipisci? Consequatur, esse. Minus neque quas veniam deleniti vitae asperiores dolorem inventore adipisci sint nam
-      maiores veritatis tenetur officiis quaerat praesentium saepe provident, autem natus corrupti sapiente, atque
-      harum! Adipisci dolorum perspiciatis est, architecto illo repudiandae consectetur tenetur neque illum
-      reprehenderit odit similique enim ab excepturi id explicabo ea vitae suscipit reiciendis atque saepe consequatur.
-      Quia rem quo, similique optio quod quasi vitae dolores fuga magnam culpa! Repellendus consequatur recusandae,
-      molestiae accusamus corporis qui reprehenderit eaque veniam praesentium ratione. Neque vitae, voluptate molestiae
-      dolor ducimus maxime pariatur repudiandae officia quas cum nesciunt ipsa fugiat veniam eius? Animi nobis expedita
-      error dolore, impedit, voluptas saepe obcaecati exercitationem quaerat alias quasi molestias sapiente commodi
-      consequuntur sunt, debitis laudantium corporis aliquid. Veritatis adipisci corporis aut exercitationem sequi velit
-      iusto voluptatem ex incidunt! Consequuntur a quas doloribus quisquam nemo, cumque modi. Harum dolor rerum
-      blanditiis eius ab ipsam nulla corrupti, est obcaecati accusamus non facere veritatis quibusdam recusandae,
-      mollitia totam consectetur omnis, aut accusantium ducimus dolore ad dolores illo! Ut maxime doloremque alias
-      itaque nam labore blanditiis cum, provident culpa? Sequi, cum repellat quae quibusdam molestiae laborum
-      voluptatibus voluptate deserunt!</p>
+    <el-row :gutter="12">
+      <el-col :span="6">
+        <el-card>1</el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>1</el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>1</el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>1</el-card>
+      </el-col>
+    </el-row>
   </section>
 </template>
 
 <script setup></script>
 
-<style lang="css">
+<style lang="css" scoped>
 .wrapper {
   padding: 10px 28px;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
-
+.el-card {
+  
+}
 .auth__block {
   display: flex;
   gap: 10px;
