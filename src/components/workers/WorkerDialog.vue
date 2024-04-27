da19f5ee151fe20aa2f5552d9348841f460689ac
bw007 <abdulaziz00791@gmail.com>
2024-04-27 08:32:35 +0500

    Workers dialog

---
+++ src/components/workers/WorkerDialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-dialog
+    v-model="toggle"
+    title="Yangi xodim"
+    width="500"
+    :before-close="handleClose"
+  >
+    <span>This is a message</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialog.setToggle(false)">Cancel</el-button>
+        <el-button type="primary" @click="dialog.setToggle(false)">
+          Confirm
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+import { dialogStore } from '@/stores/utils/dialog';
+import { storeToRefs } from 'pinia';
+
+const dialog = dialogStore();
+const { toggle } = storeToRefs(dialog);
+
+const handleClose = () => {
+  dialog.setToggle(false);
+}
+</script>
+
+<style lang="css" scoped>
+  
+</style>
