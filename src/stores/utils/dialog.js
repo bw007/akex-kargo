0168a6cce13c8e23f77b0fb76d7fbfbcc4cd98e8
bw007 <abdulaziz00791@gmail.com>
2024-04-27 08:32:45 +0500

    Dialog store

---
+++ src/stores/utils/dialog.js
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const dialogStore = defineStore("dialogStore", () => {
+  const toggle = ref(false)
+
+  const setToggle = (value) => {
+    toggle.value = value
+  }
+
+  return {
+    toggle,
+
+    setToggle
+  }
+})
