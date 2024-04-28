081ab9e5a9b21c531b5a86420250c3934185ad1f
bw007 <abdulaziz00791@gmail.com>
2024-04-29 00:28:01 +0500

    remove token store

--- src/stores/auth/auth.js
+++ src/stores/auth/auth.js
@@ -1,30 +1,58 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import { apiStore } from "../utils/api";
+import { ElMessage } from "element-plus";
+import router from "@/router";
+import cookies from "vue-cookies"
+import { loadingStore } from "../utils/loading";
 
 export const authStore = defineStore("authStore", () => {
   const api = apiStore()
-  const token = ref('');
-
-  const setToken = (value) => {
-    token.value = value;
-  }
+  const loading_store = loadingStore()
 
   const user = ref({})
   
-  const login = async (data) => {
+  // Login
+  const signIn = async (data) => {
     let res = await api.post({ url: "signin", data });
 
     if (res.status == 200) {
-      console.log(res.data);
+      ElMessage({
+        type: "success",
+        message: "Muvaffaqiyatli tizimga kirildi"
+      })
+      setTimeout(() => {
+        loading_store.setLoading(false)
+      }, 1000);
+      cookies.set("token", res.data.accessToken)
+      setTimeout(() => {
+        router.push("/")
+      }, 1500);
+    }
+  }
+
+  // Registration
+  const signUp = async (data) => {
+    let res = await api.post({ url: "signup", data })
+    
+    if (res.status == 201) {
+      ElMessage({
+        type: "success",
+        message: "Muvaffaqiyatli ro'yxatdan o'tildi"
+      })
+      setTimeout(() => {
+        loading_store.setLoading(false)
+      }, 1000);
+      setTimeout(() => {
+        router.push("/auth/signin")
+      }, 1500);
     }
   }
 
   return {
-    token,
     user,
 
-    setToken,
-    login
+    signIn,
+    signUp
   }
 })
