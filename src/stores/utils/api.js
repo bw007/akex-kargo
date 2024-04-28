91b30daa12edd4ee3c5728ec06a24c3e506f7f83
bw007 <abdulaziz00791@gmail.com>
2024-04-28 09:16:39 +0500

    actions for api requests

--- src/stores/utils/api.js
+++ src/stores/utils/api.js
@@ -1,5 +1,63 @@
-import { defineStore } from "pinia";
+import axios from "axios";
+import { defineStore, storeToRefs } from "pinia";
+import { url } from "./env";
+import { authStore } from "../auth/auth"
 
 export const apiStore = defineStore("apiStore", () => {
-  
+
+  const auth_store = authStore();
+  const { token } = storeToRefs(auth_store);
+
+  // GET
+  const get = async (payload) => {
+    return await axios.get(`${url}/${payload.url}`, {
+      headers: {
+        "Authorization": `Bearer ${token.value}`
+      },
+      params: payload.params
+    }).catch(e => {
+      console.log(e);
+    })
+  }
+
+  // POST
+  const post = async (payload) => {
+    return await axios.post(`${url}/${payload.url}`, payload.data, {
+      headers: {
+        "Authorization": `Bearer ${token.value}`
+      }
+    }).catch(e => {
+      console.log(e);
+    })
+  }
+
+  // PUT
+  const put = async (payload) => {
+    return await axios.put(`${url}/${payload.url}`, payload.data, {
+      headers: {
+        "Authorization": `Bearer ${token.value}`
+      }
+    }).catch(e => {
+      console.log(e);
+    })
+  }
+
+  // DELETE
+  const del = async (payload) => {
+    return await axios.delete(`${url}/${payload.url}`, {
+      headers: {
+        "Authorization": `Bearer ${token.value}`
+      }
+    }).catch(e => {
+      console.log(e);
+    })
+  }
+
+  return {
+    get,
+    post,
+    put,
+    del
+  }
+
 })
