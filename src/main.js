7c1b848d5ba1713392032cdaf456b3e04122060a
bw007 <abdulaziz00791@gmail.com>
2024-04-28 09:12:57 +0500

    add vue cookies

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import "./assets/styles/main.css"
+import './styles/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia';
+import { createPinia } from 'pinia'
+import VueCookies from 'vue-cookies'
+
 import App from './App.vue'
 
-import router from "./router";
+import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
@@ -16,7 +18,8 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
+app.use(VueCookies, { expires: '5s' })
 app.use(createPinia())
 app.use(ElementPlus)
 app.use(router)
-app.mount("#app")
+app.mount('#app')
