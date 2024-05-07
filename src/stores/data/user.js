480cb5a35cf7744a64b7e07d0eb3d09f8e029b8b
bw007 <mr.abdulaziz00791@gmail.com>
2024-05-07 14:33:54 +0500

    get all users method

--- src/stores/data/user.js
+++ src/stores/data/user.js
@@ -4,24 +4,57 @@ import { apiStore } from "../utils/api";
 import { ElMessage } from "element-plus";
 
 export const userStore = defineStore("userStore", () => {
-  const user = ref({})
+  const users = ref([])
   const api = apiStore()
 
+  // Add user
   const addUser = async (data) => {
     let res = await api.post({ url: "signup", data })
 
     if (res.status == 201) {
-      user.value = { ...res.data.user }
       ElMessage({
         type: "success",
         message: "Muvaffaqiyatli qo'shildi"
       })
+      users.value = [ { ...res.data.user }, ...users.value ]
     }
   }
 
-  return {
-    user,
+  // Get all users
+  const getAllUsers = async () => {
+    let res = await api.get({ url: "users" })
 
-    addUser
+    if (res.status == 200) {
+      users.value = [ ...res.data ]
+      console.log(users.value);
+    }
+  }
+  
+  // Get user
+  // const getUser = async (id) => {
+  //   return await api.get({ url: "users" })
+  // }
+
+  // Update user
+  // const updateUser = async (id) => {
+
+  // }
+
+  // Remove user
+  // const removeUser = async (id) => {
+
+  // }
+
+  // Change status user
+  // const changeStatusUser = (id) => {
+
+  // }
+
+  return {
+    users,
+
+    // getUser,
+    addUser,
+    getAllUsers
   }
 })
