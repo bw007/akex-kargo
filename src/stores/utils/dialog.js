fb7d1c8c5dbe861bc02fd8e1c45e939d1aa53ad8
bw007 <abdulaziz00791@gmail.com>
2024-05-14 23:33:57 +0500

    Edit toggler

--- src/stores/utils/dialog.js
+++ src/stores/utils/dialog.js
@@ -3,14 +3,20 @@ import { ref } from "vue";
 
 export const dialogStore = defineStore("dialogStore", () => {
   const toggle = ref(false)
+  const editToggle = ref(false)
 
   const setToggle = (value) => {
     toggle.value = value
   }
+  const setEditToggle = (value) => {
+    editToggle.value = value
+  }
 
   return {
     toggle,
+    editToggle,
     
-    setToggle
+    setToggle,
+    setEditToggle
   }
 })
