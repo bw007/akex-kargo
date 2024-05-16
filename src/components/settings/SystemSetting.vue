eedecb832eb18ee5439e095e02932fd9fcb53ded
bw007 <mr.abdulaziz00791@gmail.com>
2024-05-16 10:57:21 +0500

    system setting component

---
+++ src/components/settings/SystemSetting.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-card>
+    <template #header>
+      <div class="card-header">
+        dsd
+      </div>
+    </template>
+    <span>Hello</span>
+    <template #footer>
+      dsd
+    </template>
+  </el-card>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped>
+@import url("@/styles/components/settings/system-setting.css");
+</style>
