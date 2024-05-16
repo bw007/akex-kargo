fbe367aa97f5b8788ac8b0de9b9d164ee54c8fb9
bw007 <abdulaziz00791@gmail.com>
2024-05-16 08:31:58 +0500

    Setting view

---
+++ src/views/SettingView.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <!-- <RouterView /> -->
+    <SystemSetting />
+    <el-text>Profil</el-text>
+    <ProfileSetting />
+  </section>
+</template>
+
+<script setup>
+// import { RouterView } from 'vue-router';
+import ProfileSetting from '@/components/setting/ProfileSetting.vue'
+import SystemSetting from '@/components/setting/SystemSetting.vue'
+</script>
+
+<style lang="css" scoped>
+@import url("@/styles/components/settings/setting.css");
+</style>
