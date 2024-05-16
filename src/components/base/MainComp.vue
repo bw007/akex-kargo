5ebc129c74bdac150dc9477b3a2c8bf872fe3ab7
bw007 <mr.abdulaziz00791@gmail.com>
2024-05-16 10:56:38 +0500

    file name and other minor fixes

---
+++ src/components/base/MainComp.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-main>
+    <el-scrollbar>
+      <h2 class="main-title">{{ $route.meta.title }}</h2>
+      <RouterView />
+    </el-scrollbar>
+  </el-main>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<style lang="css" scoped>
+@import url("@/styles/components/main/main-comp.css");
+</style>
