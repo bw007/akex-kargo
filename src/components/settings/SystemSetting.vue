fdc08ac8a26861632889d4e3a33c6d63705012ee
bw007 <abdulaziz00791@gmail.com>
2024-05-18 21:47:46 +0500

    Test

--- src/components/settings/SystemSetting.vue
+++ src/components/settings/SystemSetting.vue
@@ -1,15 +1,36 @@
 <template>
   <el-card>
     <template #default>
-      <div class="card-header">
-        <el-text tag="b">Mavzu:</el-text>
-        <el-switch
-          v-model="theme"
-          :active-action-icon="Moon"
-          :inactive-action-icon="SunIcon"
-          style="--el-switch-on-color: #95d475"
-        />
-      </div>
+      <el-row class="card-header">
+        <el-col>
+          <el-text tag="b">Yuqori qism:</el-text>
+          <el-switch
+            v-model="theme.header"
+            :active-action-icon="Moon"
+            :inactive-action-icon="SunIcon"
+            style="--el-switch-on-color: #95d475"
+          />
+        </el-col>
+        <el-col>
+          <el-text tag="b">Yon panel:</el-text>
+          <el-switch
+            @change="handleChange"
+            v-model="theme.sidebar"
+            :active-action-icon="Moon"
+            :inactive-action-icon="SunIcon"
+            style="--el-switch-on-color: #95d475"
+          />
+        </el-col>
+        <el-col>
+          <el-text tag="b">Asosiy qism:</el-text>
+          <el-switch
+            v-model="theme.main"
+            :active-action-icon="Moon"
+            :inactive-action-icon="SunIcon"
+            style="--el-switch-on-color: #95d475"
+          />
+        </el-col>
+      </el-row>
     </template>
   </el-card>
 </template>
@@ -19,8 +40,10 @@ import { Moon } from '@element-plus/icons-vue';
 import { SunIcon } from '@heroicons/vue/24/solid';
 import { ref } from 'vue';
 
-const theme = ref(false)
-
+const theme = ref({})
+const handleChange = (val) => {
+  console.log(val);
+}
 
 </script>
 
