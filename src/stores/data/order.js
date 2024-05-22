8cf004ba981c4cc39fe8a2bde7b8ae6f793ca778
bw007 <mr.abdulaziz00791@gmail.com>
2024-05-22 16:10:35 +0500

    test api

--- src/stores/data/order.js
+++ src/stores/data/order.js
@@ -12,18 +12,28 @@ export const orderStore = defineStore("orderStore", () => {
   const addOrder = async (data) => {
     let res = await api.post({ url: "orders", data })
 
-    if (res.data == 201) {
+    if (res.status == 201) {
       console.log(res.data);
+      orders.value = [ ...orders.value, ...res.data ]
+      console.log(orders.value);
     }
   }
 
+  // All orders
+  const getAllOrders = async () => {
+    let res = await api.get({ url: "orders" })
 
+    if (res.status == 200) {
+      console.log(res.data);
+    }
+  }
 
   return {
     orders,
     order,
 
-    addOrder
+    addOrder,
+    getAllOrders
   }
 
 })
