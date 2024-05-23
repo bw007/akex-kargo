d790f155d43daed8e2f59049831481cae22e00dc
bw007 <mr.abdulaziz00791@gmail.com>
2024-05-23 16:54:03 +0500

    tabular data has been inserted

--- src/components/orders/OrdersTable.vue
+++ src/components/orders/OrdersTable.vue
@@ -1,50 +1,59 @@
 <template>
   <el-table empty-text="Ma'lumot yo'q" :data="filterOrderData" style="width: 100%; height: calc(100vh - 200px);">
     <el-table-column fixed type="index" align="center" />
-    <el-table-column label="Ism, Familiya" min-width="180" prop="firstName">
-    </el-table-column>
-    <el-table-column label="Email" min-width="220" prop="email">
+    <el-table-column label="Buyurtmachi" min-width="160" prop="firstName">
       <template #default="list">
-        <el-text>{{ list.row.email }}</el-text>
+        <el-text tag="b">{{ list.row.firstName }} {{ list.row.lastName }}</el-text>
       </template>
     </el-table-column>
-    <el-table-column label="Qo'shilgan sana" min-width="145" prop="createdTime">
+    <el-table-column label="ID" prop="id">
+      <template #default="list">
+        <el-text type="primary">#{{ list.row.phone.slice(4, 9) }}</el-text>
+      </template>
+    </el-table-column>
+    <el-table-column label="Mahsulot nomi" min-width="180" prop="name">
+      <template #default="list">
+        <el-text>{{ list.row.name }}</el-text>
+      </template>
+    </el-table-column>
+    <el-table-column label="Qabul sanasi" min-width="145" prop="createdTime">
       <template #default="list">
         <el-text>{{ list.row.createdTime }}</el-text>
       </template>
     </el-table-column>
-    <el-table-column label="Holati" min-width="72" prop="status">
+    <el-table-column label="Telefon raqami" min-width="130" prop="status">
       <template #default="list">
-        <el-button @click="user_store.changeStatus(list.row)" :disabled="['@super_admin'].includes(list.row.role)" :type="list.row.status ? 'success' : 'warning'">
-          <el-icon :size="16">
-            <LockOpenIcon v-if="list.row.status" />
-            <LockClosedIcon v-else />
-          </el-icon>
-        </el-button>
+        <el-text>
+          ({{ list.row.phone.slice(0, 2) }})
+          {{ list.row.phone.slice(2, 5) }}-{{ list.row.phone.slice(5, 7) }}-{{ list.row.phone.slice(7, 9) }}
+        </el-text>
       </template>
     </el-table-column>
-    <el-table-column label="Lavozim" min-width="125" prop="role">
+    <el-table-column label="Narxi" prop="price" min-width="120">
       <template #default="list">
-        <el-text type="success" v-if="list.row.role == '@super_admin'">Super admin</el-text>
-        <el-text v-else-if="list.row.role == 1">Operator</el-text>
-        <el-text v-else>Admin</el-text>
+        <el-text>{{ list.row.price.toLocaleString() }}</el-text>          
       </template>
     </el-table-column>
-    <el-table-column align="right" min-width="195">
+    <el-table-column label="Holati" min-width="125" prop="role">
+      <template #default="list">
+        <el-text v-if="list.row.status == 0">Yangi</el-text>
+        <el-text v-if="list.row.status == 1">Jarayonda</el-text>
+        <el-text v-if="list.row.status == 2">Bekor qilingan</el-text>
+      </template>
+    </el-table-column>
+    <el-table-column align="right" min-width="145">
       <template #header>
         <el-input v-model="search" placeholder="Qidirish..." />
       </template>
       <template #default="list">
         <router-link class="profil-link" :to="{ name: 'workersProfile', params: { id: list.row.id } }">
           <el-text type="primary">
-            Profil
+            Havola
             <el-icon size="12">
               <top-right />
             </el-icon>
           </el-text>
         </router-link>
-        <el-button @click="edit(list.row.id)" title="Tahrirlash" :icon="Edit" type="primary" plain />
-        <el-button @click="user_store.removeUser(list.row.id)" :disabled="['@super_admin'].includes(list.row.role)" title="O'chirish" :icon="Delete" type="danger" plain />
       </template>
     </el-table-column>
   </el-table>
@@ -52,8 +61,6 @@
 
 <script setup>
 import { computed, ref } from 'vue';
-import { Delete, Edit } from '@element-plus/icons-vue';
-import { LockClosedIcon, LockOpenIcon } from "@heroicons/vue/24/outline";
 import { storeToRefs } from 'pinia';
 import { convertDate } from '@/stores/utils/helper';
 import { orderStore } from '@/stores/data/order';
