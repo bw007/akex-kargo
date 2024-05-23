d6c1eda08eb293187171784ac037e79d12dc3e7b
bw007 <abdulaziz00791@gmail.com>
2024-05-23 07:49:06 +0500

    get orders by watch

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -14,8 +14,20 @@
 import OrdersTable from "@/components/orders/OrdersTable.vue"
 import OrderDialog from "@/components/orders/OrderDialog.vue"
 import { dialogStore } from "@/stores/utils/dialog";
+import { watch } from "vue";
+import { orderStore } from "@/stores/data/order";
+import { authStore } from "@/stores/auth/auth";
+import { storeToRefs } from "pinia";
 
 const dialog_store = dialogStore()
+const order_store = orderStore()
+const auth_store = authStore()
+const { user } = storeToRefs(auth_store)
+
+watch(user, () => {
+  order_store.getAllOrders(user.value.id)
+})
+
 </script>
 
 <style lang="css" scoped>
