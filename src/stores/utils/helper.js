3786b86eca14ff156a782fe40b162800b133c42f
bw007 <abdulaziz00791@gmail.com>
2024-05-25 10:30:10 +0500

    get full time

--- src/stores/utils/helper.js
+++ src/stores/utils/helper.js
@@ -1,6 +1,7 @@
 const addZero = (d) => d < 10 ? `0${d}` : d;
 
-export const convertDate = (d) => {
+export const convertDate = (d, type) => {
   let date = new Date(d);
-  return `${addZero(date.getDay())}-${addZero(date.getMonth() + 1)}-${date.getFullYear()}`
+  return type == 'full' ? `${addZero(date.getDay())}-${addZero(date.getMonth() + 1)}-${date.getFullYear()} | ${addZero(date.getHours())}:${addZero(date.getMinutes())}`
+    : `${addZero(date.getDay())}-${addZero(date.getMonth() + 1)}-${date.getFullYear()}`
 }
