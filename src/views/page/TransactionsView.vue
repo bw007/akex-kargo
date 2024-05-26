f025631fe79797a7756c21ce950f71d289e611ca
bw007 <abdulaziz00791@gmail.com>
2024-05-26 18:04:10 +0500

    Transaction view

--- src/views/page/TransactionsView.vue
+++ src/views/page/TransactionsView.vue
@@ -1,13 +1,17 @@
 <template>
   <section>
-    {{ $route.name }}
+    <el-row class="add-order" justify="space-between">
+      <el-text>{{ $route.meta.title }} ro'yxati</el-text>
+      <el-button @click="dialog_store.setToggle(true)" icon="Plus" type="success">Yangi</el-button>
+    </el-row>
+    <TransactionsTable />
   </section>
 </template>
 
 <script setup>
-
+import TransactionsTable from "@/components/transactions/TransactionsTable.vue"
 </script>
 
 <style lang="css" scoped>
-
+@import url("@/styles/components/transactions/transaction.css");
 </style>
