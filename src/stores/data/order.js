d87d3632378e6f86ab08a3d73e5db4b8605017d0
bw007 <mr.abdulaziz00791@gmail.com>
2024-05-27 16:48:37 +0500

    add payment in order

--- src/stores/data/order.js
+++ src/stores/data/order.js
@@ -3,33 +3,42 @@ import { apiStore } from "../utils/api";
 import { ref } from "vue";
 import { ElMessage } from "element-plus";
 import { loadingStore } from "../utils/loading";
+import { paymentStore } from "./payment";
 
 export const orderStore = defineStore("orderStore", () => {
   const api = apiStore();
   const loading_store = loadingStore()
+  const payment_store = paymentStore()
   
   const orders = ref([]);
   const order = ref({});
 
   // Add order
   const addOrder = async (data) => {
-    console.log(data);
     let res = await api.post({ url: "orders", data })
     if (res.status == 201) {
       ElMessage({
         type: 'success',
         message: "Muvaffaqiyatli qo'shildi"
       })
-      console.log(res.data);
-      orders.value = [ ...orders.value, { ...res.data } ]
-      console.log(orders.value);
+      if (res.data.payment) {
+        payment_store.addPayment({
+          order: res.data.name,
+          money: res.data.payment,
+          client: res.data.firstName + ' ' + res.data.lastName,
+          createdTime: new Date(),
+          userId: res.data.userId,
+          orderId: res.data.id
+        })
+      }
+      orders.value = [ { ...res.data }, ...orders.value ]
     }
   }
 
   // All orders
   const getAllOrders = async (userId) => {
     loading_store.setLoading(true)
-    let res = await api.get({ url: `orders?userId=${userId}` })
+    let res = await api.get({ url: `orders?_sort=id&_order=desc&userId=${userId}` })
     orders.value = []
     if (res.status == 200) {
       orders.value = [ ...res.data ]
@@ -77,5 +86,4 @@ export const orderStore = defineStore("orderStore", () => {
     updateOrder,
     getOrder
   }
-
 })
